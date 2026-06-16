5b3c4d380766f6f3223aeb88bbf1700bc3e120f1
Ninjasuperestudioso <jocamposra@est.utn.ac.cr>
2026-06-16 10:08:25 -0600

    se modifican los archivos package.json y el archivo app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+
+const NAME = process.env.SERVER_NAME;
+const VERSION = process.env.SERVER_VERSION;
+const DESCRIPTION = process.env.SERVER_DESCRIPTION;
+const PORT = process.env.SERVER_PORT || 3000;
+app.use(express.json());
+app.use(express.json());
+
 
 app.get("/", (req, res) => {
-  res.send("<h1>proyecto corriendo</h1>");
+  res.json({
+    name: NAME,
+    version: VERSION,
+    description: DESCRIPTION,
+    puerto: PORT
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto http://localhost:${port}`);
-}); 
+app.listen(PORT, () => {
+  console.log(`${NAME} version ${VERSION} corriendo en http://localhost:${PORT}`);
+});
 
 export default app;
